b29915f8ccd4f645affb6cff952644e293ad00e3
Nikhil <nikhilbatiyal3891@gmail.com>
2026-06-22 10:02:12 +0530

    first commit

---
+++ Backend/models/User.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    userName : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    password : {
+        type : String,
+        required : true,
+    },
+    role : {
+        type : String,
+        default : 'user'
+    }
+}, { timestamps: true })
+
+const User = mongoose.model('User',UserSchema);
+
+// module.exports = User   //We are using ES module syntax so we cannot export like this instead of this we use 
+export default User
