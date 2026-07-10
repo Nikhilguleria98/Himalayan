d77240101e690f40047f3e7e9c6a06f326316216
Gurnoor Singh <70206041+gurnoor926@users.noreply.github.com>
2026-07-10 14:30:04 +0530

    blog image file filter added -gurnoor

--- Backend/helpers/cloudinary.js
+++ Backend/helpers/cloudinary.js
@@ -41,8 +41,23 @@ export async function imageUploadUtil(fileOrFiles) {
   return Promise.all(files.map(uploadBuffer));
 }
 
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = [
+    "image/jpeg",
+    "image/png",
+    "image/webp",
+    "image/jpg",
+  ];
+
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only JPG, JPEG, PNG and WEBP images are allowed."));
+  }
+};
 export const upload = multer({
   storage,
+  fileFilter,
   limits: {
     files: 4, // Maximum 4 files
     fileSize: 5 * 1024 * 1024, //5 MB per image
