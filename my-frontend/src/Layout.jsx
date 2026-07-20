615f13f82e3da55327ba90b3b1ce4d5173df441f
Nikhil <nikhilbatiyal3891@gmail.com>
2026-07-20 17:12:33 +0530

    scroll to top property added

--- my-frontend/src/Layout.jsx
+++ my-frontend/src/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Providers from './components/GlobalComp/providers';
 import LayoutWrapper from './components/GlobalComp/LayoutWrapper';
+import ScrollToTop from './Scroll';
 
 export default function Layout() {
   return (
     <Providers>
       <LayoutWrapper>
+        <ScrollToTop /> {/* Renders nested route components */}
         <Outlet /> {/* Renders nested route components */}
       </LayoutWrapper>
     </Providers>
